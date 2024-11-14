f2f065eb62c744362376df9b1545f2371b5f20a1
Hyojo <samohindenis@yandex.ru>
2024-11-14 11:46:38 +0400

    edit_2 config_reader.py

--- config_reader.py
+++ config_reader.py
@@ -5,8 +5,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     bot_token: SecretStr
     api_weather_token: SecretStr
-    model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
-    print('1')
+
+    # model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
     class Config:
         env_prefix = ""
 
